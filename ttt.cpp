6c6238334a18233db9d4f36fab4a6fa4fd1ac4ff
tanishchugh01 <tanishchugh01@gmail.com>
2022-01-07 01:46:38 +0530

    improved logic and fixed clearscr bugs

--- ttt.cpp
+++ ttt.cpp
@@ -17,15 +17,15 @@ int main()
 
 void start_menu()
 {
-    system("cls");
-    
-    cout << "\n\n\tTic Tac Toe\n\n";
-    
     int continu;
     int singlevsdouble;
     
     do
     {
+        system("cls");
+        
+        cout << "\n\n\tTic Tac Toe\n\n";
+        
         char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
         
         cout<<"1.)Two Player game"<<endl;
@@ -37,6 +37,7 @@ void start_menu()
         {
             case 1:twoplayer(square);             break;
             case 2:singleplayer(square);          break;
+           
             default:cout<<"Invalid input";
                     start_menu();
         }
@@ -80,33 +81,40 @@ void singleplayer(char square[])
     
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
+        // if (choice == 1 && square[1] == '1')
 
-            square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
+        //     square[1] = mark;
+        // else if (choice == 2 && square[2] == '2')
 
-            square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
+        //     square[2] = mark;
+        // else if (choice == 3 && square[3] == '3')
 
-            square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
+        //     square[3] = mark;
+        // else if (choice == 4 && square[4] == '4')
 
-            square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
+        //     square[4] = mark;
+        // else if (choice == 5 && square[5] == '5')
 
-            square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
+        //     square[5] = mark;
+        // else if (choice == 6 && square[6] == '6')
 
-            square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
+        //     square[6] = mark;
+        // else if (choice == 7 && square[7] == '7')
 
-            square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
+        //     square[7] = mark;
+        // else if (choice == 8 && square[8] == '8')
 
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
+        //     square[8] = mark;
+        // else if (choice == 9 && square[9] == '9')
 
-            square[9] = mark;
+        //     square[9] = mark;
+        if(choice>0  &&  choice<10)
+        {
+            if(square[choice]== ('0'+choice) )
+            {
+                square[choice]=mark;
+            }
+        }
         else
         {
             cout<<"Invalid move ";
@@ -116,6 +124,7 @@ void singleplayer(char square[])
             cin.ignore();
             cin.get();
         }
+        
         i=checkwin(square);
 
         player++;
@@ -165,33 +174,41 @@ void twoplayer(char square[])
 
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
+        // if (choice == 1 && square[1] == '1')
 
-            square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
+        //     square[1] = mark;
+        // else if (choice == 2 && square[2] == '2')
 
-            square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
+        //     square[2] = mark;
+        // else if (choice == 3 && square[3] == '3')
 
-            square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
+        //     square[3] = mark;
+        // else if (choice == 4 && square[4] == '4')
 
-            square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
+        //     square[4] = mark;
+        // else if (choice == 5 && square[5] == '5')
 
-            square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
+        //     square[5] = mark;
+        // else if (choice == 6 && square[6] == '6')
 
-            square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
+        //     square[6] = mark;
+        // else if (choice == 7 && square[7] == '7')
 
-            square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
+        //     square[7] = mark;
+        // else if (choice == 8 && square[8] == '8')
 
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
+        //     square[8] = mark;
+        // else if (choice == 9 && square[9] == '9')
 
-            square[9] = mark;
+        //     square[9] = mark;
+        
+        if(choice>0  &&  choice<10)
+        {
+            if(square[choice]== ('0'+choice) )
+            {
+                square[choice]=mark;
+            }
+        }
         else
         {
             cout<<"Invalid move ";
@@ -224,36 +241,45 @@ void twoplayer(char square[])
 int checkwin(char square[])
 {
     if (square[1] == square[2] && square[2] == square[3])
-
+    {
         return 1;
+    }
     else if (square[4] == square[5] && square[5] == square[6])
-
+    {
         return 1;
+    }
     else if (square[7] == square[8] && square[8] == square[9])
-
+    {
         return 1;
+    }
     else if (square[1] == square[4] && square[4] == square[7])
-
+    {   
         return 1;
+    }
     else if (square[2] == square[5] && square[5] == square[8])
-
+    {
         return 1;
+    }
     else if (square[3] == square[6] && square[6] == square[9])
-
+    {
         return 1;
+    }
     else if (square[1] == square[5] && square[5] == square[9])
-
+    {
         return 1;
+    }
     else if (square[3] == square[5] && square[5] == square[7])
-
+    {
         return 1;
+    }
     else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-                    && square[4] != '4' && square[5] != '5' && square[6] != '6'
-                  && square[7] != '7' && square[8] != '8' && square[9] != '9')
-
+            && square[4] != '4' && square[5] != '5' && square[6] != '6'
+            && square[7] != '7' && square[8] != '8' && square[9] != '9')
+    {
         return 0;
-    else
-        return -1;
+    }
+    
+    return -1;
 }
 
 void board(char square[])
